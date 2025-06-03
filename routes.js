b2e99fcf5f0b47e9b6f0581d980972a9fa2ee778
Rohit Kumar <rohitkumar20042004@gmail.com>
2025-06-03 21:57:25 +0530

    updated

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const controller = require('./controller.js')
+const controller = require("./controller.js");
 
-router.post('/create',controller.createUser);
-router.get('/',controller.getCustomer);
-router.patch('/:id',controller.customerupdate);
-router.delete('/:id',controller.customerDelete);
+router.post("/create", controller.createUser);
+router.get("/", controller.getCustomer);
+router.patch("/:id", controller.customerupdate);
+router.delete("/:id", controller.customerDelete);
 
 module.exports = router;
