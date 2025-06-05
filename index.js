1f372aae542a4afdf58c4c64fbb6fdf280a99962
Rohit Kumar <rohitkumar20042004@gmail.com>
2025-06-05 21:48:34 +0530

    updated

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDb = require('./Db.js')
-const cusRoute = require('./routes.js')
-const app= express();
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDb = require("./Db.js");
+const cusRoute = require("./routes.js");
+const app = express();
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 dotenv.config();
 
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
 connectDb();
-app.get('/',(req,res)=>{
-    console.log("Request send on / route");
-    return res.status(200).json({
-        message:"ALL GOOD!!!!"
-    })
-})
-app.use('/cus',cusRoute)
+app.get("/", (req, res) => {
+  console.log("Request send on / route");
+  return res.status(200).json({
+    message: "ALL GOOD!!!!",
+  });
+});
+app.use("/cus", cusRoute);
 
-app.listen(PORT,()=>{
-    console.log(`Server has strated running at ${PORT} PORT`);
-})
+app.listen(PORT, () => {
+  console.log(`Server has strated running at ${PORT} PORT`);
+});
 
 module.exports = app;
