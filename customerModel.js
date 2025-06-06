70655006a8dcdb5c5bef65a87b9c9f96bcc9e5c7
Rohit Kumar <rohitkumar20042004@gmail.com>
2025-06-06 21:47:55 +0530

    updated

--- customerModel.js
+++ customerModel.js
@@ -1,20 +1,20 @@
-const mongoose = require ('mongoose')
+const mongoose = require("mongoose");
 
 const customerSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    age:{
-        type:Number,
-        required:true
-    },
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+});
 
-const customerModel = mongoose.model('Customer',customerSchema);
+const customerModel = mongoose.model("Customer", customerSchema);
 
 module.exports = customerModel;
