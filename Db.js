451decd8af1b086e024b3d04c839b0c2bde609e5
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-06-19 23:23:32 +0530

    All the api implemented and done

--- Db.js
+++ Db.js
@@ -2,4 +2,16 @@ const mongoose = require('mongoose');
 const dotenv= require('dotenv');
 
 dotenv.config()
-const url= process.env.MONGODB_URl
+const url= process.env.MONGODB_URl;
+
+async function connectDb(){
+    try{
+        await mongoose.connect(url);
+        console.log("DATABASE CONNECTED !!")
+    }
+    catch(e){
+        console.log("ERROR COULD NOT CONNECT TO DATABASE")
+    }
+}
+
+module.exports = connectDb;
